437b1fd89ad54855b74c0d808bc7927a7295def2
Brooke Ginn <brookeginn@icloud.com>
2022-04-03 20:31:46 -0400

    new forms

--- app.py
+++ app.py
@@ -1,29 +1,55 @@
 from crypt import methods
 from fileinput import close
 import sqlite3
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 from sqlalchemy import false
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
 from flask import Flask, render_template
 
-loggedIn = False
-
+# connection to database
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+# login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        print('running post')
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            print('checking pass')
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            print('pass accepted')
+            loggedIn = True
+            return redirect("/")
+
+# home page
 @app.route('/')
 def home():
-    if loggedIn == False:
-        return render_template('login.html')
-    else:
-        return render_template('home.html')
+    return render_template('home.html')
+
+# modify products in the table
+@app.route('/inventorymodify')
+def inventorymodify():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM inventory').fetchall()
+    conn.close()
+    return render_template('inventorymodify.html')
+    
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
 
 
-
+# main inventory page
 @app.route('/inventory')
 def inventory():
     conn = get_db_connection()
@@ -32,81 +58,50 @@ def inventory():
         print(item)
     conn.close()
     return render_template('inventory.html', inventory=inventory)
+      
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    add_inventory = cur.execute("""
+    INSERT INTO inventory (id, itemName, description, location) 
+    VALUES (?, ?, ?, ?)""",(itemNum, itemName, description, location))
+    conn.commit()
+    conn.close()
+    return redirect("/inventory")
 
 
-@app.route('/inventoryform')
-def inventoryform():
-    return render_template('inventoryform.html')
+# remove row from table and return to main inventory page
+@app.route('/inventoryremove', methods=["GET","POST"] )
+def inventoryremove():
+    if request.method == "GET":
+        return render_template("inventoryremove.html")
 
+    if request.method == "POST":
+        itemNum = request.form.get("itemNum")
+        conn = get_db_connection()
+        conn.execute('DELETE FROM inventory WHERE id = ?', (itemNum,))
+        conn.commit()
+        conn.close()
+        return redirect("/inventory")
+
+
+
+# invoices page
+@app.route('/invoices', methods=["GET", "POST"])
+def invoices():
+    if request.method == "GET":
+        return render_template('invoices.html')
+
+# add info to table and return to main inventory page
 @app.route('/inventoryadd', methods=["GET", "POST"] )
 def inventoryadd():
+    if request.method == "GET":
+        return render_template('inventoryadd.html')
+   
     if request.method == "POST":
         itemNum = request.form.get("itemNum")
         itemName = request.form.get("itemName")
         description = request.form.get("description")
         location = request.form.get("location")
-
-      
-        conn = sqlite3.connect('database.db')
-        cur = conn.cursor()
-        add_inventory = cur.execute("INSERT INTO inventory (id, itemName, description, location) VALUES (?, ?, ?, ?)", (itemNum, itemName, description, location))
-        conn.commit()
-        conn.close()
-        return render_template("inventory.html")
-
-@app.route('/invoices')
-def invoices():
-    return render_template('invoices.html')
-
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html')
-
-@app.route('/orders')
-def orders():
-    return render_template('orders.html')
-
-
-
-# editing post
-# @app.route('/inventorymodify', methods=["GET", "POST"] )
-# def inventorymodify():
-#     if request.method == "POST":
-#         itemNum = request.form.get("itemNum")
-#         itemName = request.form.get("itemName")
-#         description = request.form.get("description")
-#         location = request.form.get("location")
-#         if not itemNum:
-#             print('Item Number is required!')
-#         else:
-#             conn = sqlite3.connect('database.db')
-#             cur = conn.cursor()
-#             add_inventory = cur.execute("INSERT INTO inventory (id, itemName, description, location) VALUES (?, ?, ?, ?)", (itemNum, itemName, description, location))
-#             conn.commit()
-#             conn.close()
-#             return render_template("inventory.html")
-
-@app.route('/inventoryremove', methods=["POST"] )
-def inventorymodify(id):
-    if request.method == "POST":
-        post = get_post(id)
-        conn = get_db_connection()
-        conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-        conn.commit()
-        conn.close()
-        print('"{}" was successfully deleted!'.format(post['title']))
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            loggedIn = True
-            return render_template('home.html')
-    return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
